bd28d3d954d94d0877401142a42e583b4735b4c1
marshaerh <76280295+marshaerh@users.noreply.github.com>
2022-03-28 10:53:32 +0700

    Add files via upload

---
+++ Sekolah_1814/Sekolah_1814/KoneksiDB/Koneksi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Sekolah_1814.KoneksiDB
+{
+    class Koneksi
+    {
+        private static SqlConnection koneksi;
+
+        public static SqlConnection GetKoneksi()
+        {
+            koneksi = new SqlConnection();
+            koneksi.ConnectionString = "Data Source=WINDOWS-8MM842K\\SQLEXPRESS;" +
+                                       "Initial Catalog=Sekolah_1814;" +
+                                       "Integrated Security=True";
+
+            return koneksi;
+        }
+    }
+}
